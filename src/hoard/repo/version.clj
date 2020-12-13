21da07930cbe6750b85bdb4a37bddbd28ecb8671
Greg Look <greg@mvxcvi.com>
2020-12-13 08:31:35 -0800

    Base version ids on UTC, add parser.

--- src/hoard/repo/version.clj
+++ src/hoard/repo/version.clj
@@ -3,7 +3,10 @@
     [clojure.spec.alpha :as s]
     [hoard.repo.index :as index])
   (:import
-    java.time.LocalDateTime
+    (java.time
+      Instant
+      LocalDateTime
+      ZoneOffset)
     java.time.temporal.ChronoField))
 
 
@@ -14,27 +17,33 @@
   string?)
 
 
+;; Storage size in bytes used by the version data.
+(s/def ::size
+  nat-int?)
+
+
 ;; Time the version was created.
 (s/def ::created-at
   inst?)
 
 
 ;; Number of file entries present in the version.
-(s/def ::count
+(s/def ::tree-count
   nat-int?)
 
 
 ;; Total byte size of files represented in the version.
-(s/def ::size
+(s/def ::tree-size
   nat-int?)
 
 
 ;; Map of version metadata attributes.
 (s/def ::meta
-  (s/keys :req [::id]
-          :opt [::created-at
-                ::count
-                ::size]))
+  (s/keys :req [::id
+                ::size
+                ::created-at]
+          :opt [::tree-count
+                ::tree-size]))
 
 
 ;; Sequence of index data entries.
@@ -47,12 +56,30 @@
 
 (defn gen-id
   "Generate a new version index identifier."
-  []
-  (let [alphabet "23456789abcdefghjkmnpqrstuvwxyz"
-        now (LocalDateTime/now)]
-    (format "%04d%02d%02d-%05d-%s"
-            (.get now ChronoField/YEAR)
-            (.get now ChronoField/MONTH_OF_YEAR)
-            (.get now ChronoField/DAY_OF_MONTH)
-            (.get now ChronoField/SECOND_OF_DAY)
-            (apply str (repeatedly 5 #(rand-nth alphabet))))))
+  ([]
+   (gen-id (Instant/now)))
+  ([^Instant now]
+   (let [alphabet "23456789abcdefghjkmnpqrstuvwxyz"
+         local-now (LocalDateTime/ofInstant now ZoneOffset/UTC)]
+     (format "%04d%02d%02d-%05d-%s"
+             (.get local-now ChronoField/YEAR)
+             (.get local-now ChronoField/MONTH_OF_YEAR)
+             (.get local-now ChronoField/DAY_OF_MONTH)
+             (.get local-now ChronoField/SECOND_OF_DAY)
+             (apply str (repeatedly 5 #(rand-nth alphabet)))))))
+
+
+(defn parse-id-inst
+  "Parse a version identifier and return the instant in time it was created.
+  Returns nil if the id pattern is not recognized."
+  [id]
+  (when-let [[_ year month day seconds] (re-matches #"^(\d{4})(\d\d)(\d\d)-(\d{5})-[a-z0-9]+$" id)]
+    (let [seconds (Integer/parseInt seconds)
+          ldt (LocalDateTime/of
+                (Integer/parseInt year)
+                (Integer/parseInt month)
+                (Integer/parseInt day)
+                (int (/ seconds 3600))
+                (int (/ (mod seconds 3600) 60))
+                (int (mod seconds 60)))]
+      (.toInstant ldt ZoneOffset/UTC))))
